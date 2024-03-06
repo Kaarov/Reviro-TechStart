9d902821a6f6a9bc5e388c27d5eeb52045665a3a
Anas Kaarov <anasbinkurban@gmail.com>
2024-03-06 15:59:52 +0600

    refac: updated configuration

--- config/exceptions.py
+++ config/exceptions.py
@@ -3,6 +3,8 @@ from typing import Optional
 from django.http import Http404
 from rest_framework import status
 from rest_framework.exceptions import APIException
+from rest_framework.exceptions import NotAuthenticated
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework.views import exception_handler
 
@@ -14,6 +16,26 @@ def drf_exception_handler(exc, context):
         return Response(
             {"code": exc.code, "message": exc.message, "field_errors": []},
             status=exc.http_code,
+        )
+
+    if isinstance(exc, NotAuthenticated):
+        return Response(
+            {
+                "code": ReviroUnauthorized.code,
+                "message": ReviroUnauthorized.message,
+                "error_fields": [],
+            },
+            status=ReviroUnauthorized.http_code,
+        )
+
+    if isinstance(exc, AuthenticationFailed):
+        return Response(
+            {
+                "code": ReviroAuthenticationFailed.code,
+                "message": ReviroAuthenticationFailed.message,
+                "error_fields": [],
+            },
+            status=ReviroAuthenticationFailed.http_code,
         )
 
     if isinstance(exc, APIException):
@@ -85,3 +107,21 @@ class ReviroBadRequest(ReviroBaseException):
     code = "BadRequest"
     message = "Invalid request, please try again later"
     http_code = status.HTTP_400_BAD_REQUEST
+
+
+class ReviroPermissionDenied(ReviroBaseException):
+    code = "PermissionDenied"
+    message = "Permission Denied"
+    http_code = status.HTTP_403_FORBIDDEN
+
+
+class ReviroUnauthorized(ReviroBaseException):
+    code = "Unauthorized"
+    message = "Unauthorized"
+    http_code = status.HTTP_401_UNAUTHORIZED
+
+
+class ReviroAuthenticationFailed(ReviroUnauthorized):
+    code = "AuthenticationError"
+    message = "Authentication Error"
+    http_code = status.HTTP_401_UNAUTHORIZED
