90bd33f3aa272c44a176379379edf34cbd9c78ba
Anas Kaarov <anasbinkurban@gmail.com>
2024-03-06 16:01:36 +0600

    refac: updated views

--- establishment/views.py
+++ establishment/views.py
@@ -1,4 +1,9 @@
 from rest_framework import viewsets
+
+from config.permissions import GuestPermission
+from config.permissions import PartnerPermission
+from config.permissions import AdminUserPermission
+
 from establishment.models import Establishment
 from establishment.serializers import EstablishmentSerializer
 
@@ -6,3 +11,11 @@ from establishment.serializers import EstablishmentSerializer
 class EstablishmentModelViewSet(viewsets.ModelViewSet):
     serializer_class = EstablishmentSerializer
     queryset = Establishment.objects.all()
+
+    def get_permissions(self):
+        if self.action == 'list':
+            permission_classes = [GuestPermission]
+        else:
+            permission_classes = [PartnerPermission, AdminUserPermission]
+
+        return [permission() for permission in permission_classes]
--- product/views.py
+++ product/views.py
@@ -1,4 +1,9 @@
 from rest_framework import viewsets
+
+from config.permissions import GuestPermission
+from config.permissions import PartnerPermission
+from config.permissions import AdminUserPermission
+
 from product.models import Product
 from product.serializers import ProductSerializer
 
@@ -6,3 +11,11 @@ from product.serializers import ProductSerializer
 class ProductModelViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+
+    def get_permissions(self):
+        if self.action == 'list':
+            permission_classes = [GuestPermission]
+        else:
+            permission_classes = [PartnerPermission, AdminUserPermission]
+
+        return [permission() for permission in permission_classes]
